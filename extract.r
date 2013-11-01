3dd2fc3e50ff7250113d1756e9495e41c8151a55
mhlinder <mhlinder@gmail.com>
2013-11-01 17:45:17 -0400

    initial commit

---
+++ extract.r
@@ -0,0 +1,5 @@
+# Extract subway, NYC data from shapefile
+# http://www.nceas.ucsb.edu/scicomp/usecases/PolygonDissolveOperationsR
+
+# nybb_13a/ is from http://www.nyc.gov/html/dcp/html/bytes/dwndistricts.shtml
+# google_transit/ is from http://www.mta.info/developers/download.html
