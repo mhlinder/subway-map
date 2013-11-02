55f9f8863d777b54ad6283572dd2b6e55518a0c8
mhlinder <mhlinder@gmail.com>
2013-11-02 00:34:39 -0400

    plot each individual island comprising new york city

--- voronoi.py
+++ voronoi.py
@@ -1,5 +1,16 @@
 import pandas as pd
-from scipy.spatial import Voronoi
+import matplotlib.pyplot as plt
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 city_aea = pd.read_csv('output/city_aea')
 stops_aea = pd.read_csv('output/stops_aea')
+
+stops_voronoi = Voronoi(stops_aea[['lon','lat']])
+
+for snum in pd.unique(city_aea['SID']):
+    plt.figure(snum)
+    s = city_aea[city_aea['SID']==snum]
+    plt.plot(s['X'],s['Y'])
+    plt.scatter(stops_aea['lon'],stops_aea['lat'])
+    plt.savefig('output/%d.png' % snum)
+    plt.close()
