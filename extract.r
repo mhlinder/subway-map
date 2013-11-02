051093a3e9f7de39138c2d978a906b98635b00cb
mhlinder <mhlinder@gmail.com>
2013-11-01 22:09:41 -0400

    now dissolves all boroughs of new york into one

--- extract.r
+++ extract.r
@@ -1,5 +1,4 @@
 # Extract NYC, subway data from shapefile
-# http://www.nceas.ucsb.edu/scicomp/usecases/PolygonDissolveOperationsR
 
 # nybb_13a/ is from http://www.nyc.gov/html/dcp/html/bytes/dwndistricts.shtml
 # google_transit/ is from http://www.mta.info/developers/download.html
@@ -23,5 +22,19 @@ stops_parents <- stops[stops$location_type==1,]
 stops_parents <- SpatialPointsDataFrame(cbind(stops_parents$stop_lon,stops_parents$stop_lat),as.data.frame(stops_parents$stop_name),proj4str=CRS("+proj=longlat +datum=WGS84"))
 
 # # Project into Albers Equal Area
-city_aea <- spTransform(city,CRS('+proj=aea +lon_0=-96'))
-stops_aea <- spTransform(stops_parents,CRS('+proj=aea +lon_0=-96'))
+# City
+city_aea <- spTransform(city,CRS('+proj=aea +datum=WGS84 +lon_0=-96'))
+# Dissolve into single shape--see http://www.nceas.ucsb.edu/scicomp/usecases/PolygonDissolveOperationsR
+lps <- getSpPPolygonsLabptSlots(city_aea)
+onebin <- cut(lps[,1], range(lps[,1]), include.lowest=TRUE)
+city_aea <- unionSpatialPolygons(city_aea, onebin)
+
+# Stops
+stops_aea <- spTransform(stops_parents,CRS('+proj=aea +datum=WGS84 +lon_0=-96'))
+
+# # Convert to data frame with:
+# stops_aea_df <- as.data.frame(stops_aea)
+# colnames(stops_aea_df) <- c("stop_name","lon","lat")
+# # Convert to PolygonSet with:
+# city_aea_ps <- SpatialPolygons2PolySet(city_aea)
+# plotPolys(city_aea_ps, proj=TRUE)
