00ed8487e006af90d3173375112e08e86ddd6007
mhlinder <mhlinder@gmail.com>
2013-11-04 22:38:35 -0500

    now fully loads, converts, collects new york city boundary data

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@ import fiona
 from pyproj import Proj, transform
 from shapely.geometry import Polygon, MultiPolygon
 import numpy as np
-
+import pandas as pd
 import matplotlib.pyplot as plt
 
 with fiona.open('indata/nybb_13a/nybb.shp') as source:
@@ -25,17 +25,29 @@ with fiona.open('indata/nybb_13a/nybb.shp') as source:
             new = Polygon(p3_points)
             nyc = nyc.union(new)
 
-# there
+# i = 0
+# for shape in nyc:
+    # x,y = shape.exterior.xy
+    # plt.plot(x,y)
 
-i = 0
-for shape in nyc:
-    x,y = shape.exterior.xy
-    plt.plot(x,y)
+    # bounds = np.array(shape.bounds)
+    # bounds = bounds.reshape([2,2])
+    # cx = np.mean(bounds[:,0])
+    # cy = np.mean(bounds[:,1])
 
-    bounds = np.array(shape.bounds)
-    bounds = bounds.reshape([2,2])
-    cx = np.mean(bounds[:,0])
-    cy = np.mean(bounds[:,1])
-
-    plt.text(cx,cy,str(i))
-    i = i+1
+    # plt.text(cx,cy,str(i))
+    # i = i+1
+# By running the above, we identify the following islands that contain subway stations:
+#        i  borough            name
+#   1.   3  staten island      staten island
+#   2.  39  manhattan          manhattan
+#   3.  36  manhattan          roosevelt island
+#   4.  40  bronx              bronx
+#   5.  10  brooklyn / queens  rockaways
+#   6.  14  brooklyn / queens  jamaica bay wildlife refuge / broad channel
+#   7.  15  brooklyn / queens  brooklyn / queens
+indices = [3, 39, 36, 40, 10, 14, 15]
+boundary = []
+for i in indices:
+    boundary.append(nyc[i])
+boundary = MultiPolygon(boundary)
