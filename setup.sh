b099b0556d673d2a0ea557cbe90a6baaf547844a
mhlinder <mhlinder@gmail.com>
2013-11-10 11:59:23 -0500

    combining some files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Initialize directory structure for SubwayMap project
 
+# # Get data
 mkdir indata
 cd indata
 
@@ -26,3 +27,23 @@ wget http://www2.census.gov/acs2011_5yr/summaryfile/Sequence_Number_and_Table_Nu
 cd ..
 
 cd ..
+
+# # Format data
+# extract data from American Community Survey census estimates, 2007--2011 5yr
+# estimates, median household income.
+
+# for data documentation, see http://www2.census.gov/acs2011_5yr/summaryfile/ACS_2007_2011_SF_Tech_Doc.pdf
+data_dir='indata/ACS/'
+geo_file=$data_dir'g20115ny.csv'
+est_file=$data_dir'e20115ny0056000.txt'
+save='save/median'
+
+# extract LOGRECNOs corresponding to census tracts (SUMLEVEL 140) and only in
+# the four counties of interest (excludes staten island); GEOID for connection
+# with TIGER GIS data; and estimate of median household income
+echo 'GEOID,MEDIAN_INCOME' > $save
+awk -F, 'FNR==NR{
+            if ($3 ~ 140 && $51 ~ / (Bronx|Kings|New York|Queens) County/)
+                {locs[$5]=$49}}
+            {if ($6 in locs && $177 !~ /\./)
+                {print locs[$6] "," $177}}' $geo_file $est_file >> $save
