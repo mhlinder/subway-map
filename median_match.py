e0f988232d135ba5bbd7f98630bdea0d59fc0678
mhlinder <mhlinder@gmail.com>
2013-11-10 11:56:35 -0500

    produces stops data frame that includes income

--- median_match.py
+++ median_match.py
@@ -1,30 +1,38 @@
 # match median income data with subway stop region data
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+from descartes import PolygonPatch
 
 stops = pickle.load(open('save/stops.p','rb'))
 tracts = pickle.load(open('save/tracts.p','rb'))
+nyc = pickle.load(open('save/nyc.p','rb'))
 
-stops['xmin'] = np.tile(np.nan, len(stops))
-stops['xmax'] = np.tile(np.nan, len(stops))
-stops['ymin'] = np.tile(np.nan, len(stops))
-stops['ymax'] = np.tile(np.nan, len(stops))
+stops_list = stops['region'].tolist()
+tracts_list = tracts['region'].tolist()
+tract_sets = []
+for i in range(len(stops_list)):
+    stop = stops_list[i]
+    contains = []
+    for j in range(len(tracts_list)):
+        tract = tracts_list[j]
+        if stop.intersects(tract):
+            intersection = (stop.intersection(tract))
+            contains.append((intersection.area,tracts.iloc[j]['income']))
+    tract_sets.append(contains)
+
+# set income for each subway stop to be average median household income,
+# weighted by proportion of total area represented by each tract
+stops['income'] = np.tile(np.nan, len(stops))
 for i in range(len(stops)):
     stop = stops.iloc[i]
-    bounds = stop['region'].bounds
-    stops['xmin'].iloc[i] = bounds[0]
-    stops['xmax'].iloc[i] = bounds[2]
-    stops['ymin'].iloc[i] = bounds[1]
-    stops['ymax'].iloc[i] = bounds[3]
+    tract_set = np.array(tract_sets[i])
 
-tracts['xmin'] = np.tile(np.nan, len(tracts))
-tracts['xmax'] = np.tile(np.nan, len(tracts))
-tracts['ymin'] = np.tile(np.nan, len(tracts))
-tracts['ymax'] = np.tile(np.nan, len(tracts))
-for i in range(len(tracts)):
-    tract = tracts.iloc[i]
-    bounds = tract['region'].bounds
-    tracts['xmin'].iloc[i] = bounds[0]
-    tracts['xmax'].iloc[i] = bounds[2]
-    tracts['ymin'].iloc[i] = bounds[1]
-    tracts['ymax'].iloc[i] = bounds[3]
+    total = np.sum(tract_set[:,0])
+    weights = tract_set[:,0] / total
+    weighted_incomes = tract_set[:,1]*weights
+    income = np.sum(weighted_incomes)
+
+    stops['income'].iloc[i] = income
+
+stops['lincome'] = np.log(stops['income'])
