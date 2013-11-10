abeb64b45172a65959acef3accfb1a78cd6e6a16
mhlinder <mhlinder@gmail.com>
2013-11-10 12:00:46 -0500

    again, combining files

--- median.py
+++ median.py
@@ -6,8 +6,6 @@ from pyproj import Proj, transform
 from shapely.geometry import Polygon
 from geopandas import GeoDataFrame
 
-import pickle
-
 # read in census income data
 incomes = read_csv('save/median')
 for i in range(len(incomes)):
@@ -43,4 +41,32 @@ for i in range(len(tracts)):
     income = incomes[incomes['GEOID'] == tract['geoid']].iloc[0]['MEDIAN_INCOME']
     tracts['income'].iloc[i] = income
 
-pickle.dump(tracts,open('save/tracts.p','wb'))
+stops_list = stops['region'].tolist()
+tracts_list = tracts['region'].tolist()
+tract_sets = []
+for i in range(len(stops_list)):
+    stop = stops_list[i]
+    contains = []
+    for j in range(len(tracts_list)):
+        tract = tracts_list[j]
+        if stop.intersects(tract):
+            intersection = (stop.intersection(tract))
+            contains.append((intersection.area,tracts.iloc[j]['income']))
+    tract_sets.append(contains)
+
+# set income for each subway stop to be average median household income,
+# weighted by proportion of total area represented by each tract
+stops['income'] = np.tile(np.nan, len(stops))
+for i in range(len(stops)):
+    stop = stops.iloc[i]
+    tract_set = np.array(tract_sets[i])
+
+    total = np.sum(tract_set[:,0])
+    weights = tract_set[:,0] / total
+    weighted_incomes = tract_set[:,1]*weights
+    income = np.sum(weighted_incomes)
+
+    stops['income'].iloc[i] = income
+
+# log income
+stops['lincome'] = np.log(stops['income'])
