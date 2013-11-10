10b4d2e0f01d9f6c94bd635a19b459517eaf3962
mhlinder <mhlinder@gmail.com>
2013-11-09 21:57:45 -0500

    finds median income for eachcensus tract in new york city

--- median_extract.sh
+++ median_extract.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # for data documentation, see http://www2.census.gov/acs2011_5yr/summaryfile/ACS_2007_2011_SF_Tech_Doc.pdf
-geo_file='indata/ACS/g20115ny.csv'
+data_dir='indata/ACS/'
+geo_file=$data_dir'g20115ny.csv'
+est_file=$data_dir'e20115ny0056000.txt'
 
-# extract LOGRECNOs corresponding to census tracts (SUMLEVEL 140)
-awk -F, '$3 ~ 140 {print $5}' $geo_file
+# extract LOGRECNOs corresponding to census tracts (SUMLEVEL 140) and only in
+# the four counties of interest (excludes staten island); GEOID for connection
+# with TIGER GIS data; and estimate of median household income
+awk -F, 'FNR==NR{
+            if ($3 ~ 140 && $51 ~ / (Bronx|Kings|New York|Queens) County/)
+                {locs[$5]=$49}}
+            {if ($6 in locs && $177 !~ /\./)
+                {print $6 "," locs[$6] "," $177}}' $geo_file $est_file
