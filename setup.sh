1d0d86fb6ccf0eb8d7f4d87a508b9ee384ad51de
mhlinder <mhlinder@gmail.com>
2013-11-22 10:55:14 -0500

    changed setup.sh to actually work

--- setup.sh
+++ setup.sh
@@ -11,17 +11,20 @@ unzip nybb_13a.zip
 rm nybb_13a.zip
 
 # http://www.mta.info/developers/download.html
+mkdir google_transit
+cd google_transit
 wget http://www.mta.info/developers/data/nyct/subway/google_transit.zip
 unzip google_transit.zip
 rm google_transit.zip
+cd ..
 
 mkdir ACS
 cd ACS/
 # wget http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_All_Tables/NewYork_Tracts_Block_Groups_Only.zip
 # unzip NewYork_Tracts_Block_Groups_Only.zip
-http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/20115ny0056000.zip
+wget http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/20115ny0056000.zip
 unzip 20115ny0056000.zip
-http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/20115ny0002000.zip
+wget http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/20115ny0002000.zip
 unzip 20115ny0002000.zip
 wget http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/g20115ny.csv
 wget http://www2.census.gov/acs2011_5yr/summaryfile/2007-2011_ACSSF_By_State_By_Sequence_Table_Subset/NewYork/Tracts_Block_Groups_Only/g20115ny.txt
