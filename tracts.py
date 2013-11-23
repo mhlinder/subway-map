ef0b786b7f4dbd7fb062a81f7c9f2f3c8f56579d
mhlinder <mhlinder@gmail.com>
2013-11-23 02:02:18 -0500

    now have separate tracts for each population record, and shapes

--- tracts.py
+++ tracts.py
@@ -1,5 +1,18 @@
 import pickle
+import matplotlib.pyplot as plt
 
 nyc = pickle.load(open('save/nyc.p','rb'))
-tracts = pickle.load(open('save/nyc.p','rb'))
+tracts = pickle.load(open('save/tracts.p','rb'))
 stops = pickle.load(open('save/stops.p','rb'))
+
+# plot
+for i in range(len(tracts)):
+    tract = tracts.iloc[i]
+    region = tract['region']
+    if region.type=='MultiPolygon':
+        for subshape in region:
+            x,y = subshape.exterior.xy
+            plt.plot(x,y,'k-')
+    else:
+        x,y = tract['region'].exterior.xy
+        plt.plot(x,y,'k-')
