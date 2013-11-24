2440ad445b79daf78999fe0e293eb0ca14dc48cc
mhlinder <mhlinder@gmail.com>
2013-11-23 19:23:35 -0500

    remove correlation function

--- tracts.py
+++ tracts.py
@@ -1,4 +1,4 @@
-import pickle
+mport pickle
 import numpy as np
 import scipy.stats as stats
 
@@ -9,25 +9,5 @@ nyc = pickle.load(open('save/nyc.p','rb'))
 tracts = pickle.load(open('save/tracts.p','rb'))
 stops = pickle.load(open('save/stops.p','rb'))
 
-def corr(x,y,k):
-    print 'lag: %i' % k
-
-    if k > 0:
-        x = x[:-k]
-        y = y[k:]
-    elif k < 0:
-        k = np.abs(k)
-        x = x[k:]
-        y = y[:-k]
-
-    xbar = np.mean(x)
-    ybar = np.mean(y)
-    r = np.sum( (x-xbar)*(y-ybar) ) / np.sqrt(sum( (x-xbar)**2 ) * sum(
-        (y-ybar)**2 ))
-    print r
-    # return r
-
 x = tracts['larea']
 y = tracts['lpop_dens']
-
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
